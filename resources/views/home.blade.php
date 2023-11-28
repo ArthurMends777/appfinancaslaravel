ba72d35469aa1d08fd0e5d6e9d86c6803a6c2638
Arthur Mendonça <109369137+ArthurMends777@users.noreply.github.com>
2023-11-28 12:32:21 -0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,83 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resumo Financeiro - WebMoneyManager</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 20px;
-        }
-  
-        h1 {
-            margin-bottom: 20px;
-        }
-        h2 {
-            margin-top: 30px;
-        }
-        .totals {
-            margin-bottom: 30px;
-        }
-        ul {
-            list-style: none;
-            padding: 0;
-        }
-        li {
-            margin-bottom: 10px;
-            border-bottom: 1px solid #ccc;
-            padding-bottom: 5px;
-        }
-    </style>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container">
-            <a class="navbar-brand" href="#"> WebMoneyManager </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Página Inicial</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('transactions.create') }}">Transações</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('report.index') }}">Relatórios</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('logout') }}">Sair</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <x-header />
 
-    <div class="container mt-4">
-        <h1 class="text-center">Resumo Financeiro</h1>
-        <div class="row mt-4">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-header">
-                        Saldo Disponível
-                    </div>
-                    <div class="card-body">
-                        <h2 class="text-center">$190,000.00</h2>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-header">
-                        Despesas do Mês
-                    </div>
-                    <div class="card-body">
-                        <h2 class="text-center">$5,000.00</h2>
-                    </div>
-                </div>
-            </div>
+        <div>
+            <h3>Total de Gastos: R$ {{ $totalExpenses }}</h3>
+            <h3>Total de Ganhos: R$ {{ $totalIncome }}</h3>
+            <h3>Saldo Total: R$ {{ $totalBalance }}</h3>
         </div>
-    </div>
+        
+        <div>
+            <h2>Últimas Transações</h2>
+            <ul>
+                @foreach($transactions as $transaction)
+                    <li>
+                        {{ $transaction->description }} - {{ $transaction->amount }} - {{ $transaction->transaction_date }}
+                    </li>
+                @endforeach
+            </ul>
+        </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
